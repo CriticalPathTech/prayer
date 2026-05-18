6f703f990b73bd9732a14abeb3a71085af758d3f
Yiyang Li <yiyangli2010@gmail.com>
2026-05-17 22:16:50 -0700

    feat(web): wire Repost into MyArchivePage (desktop)

--- apps/web/src/pages/MyArchivePage.tsx
+++ apps/web/src/pages/MyArchivePage.tsx
@@ -2,6 +2,7 @@ import type { JSX } from 'react';
 import { useEffect, useState } from 'react';
 
 import { PostCard } from '../components/PostCard';
+import { useRepostFromArchive } from '../hooks/useRepostFromArchive';
 import type { FeedPost } from '../hooks/useFeed';
 import { useRepostFromArchive } from '../hooks/useRepostFromArchive';
 import { apiFetch } from '../lib/api';
